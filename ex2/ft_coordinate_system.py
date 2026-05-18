7696a21a57db6f1cbe15b97c1c79098b77623b90
Dynsai <dynsaicode.versus554@passmail.com>
2026-05-18 17:04:50 +0200

    ex2 done. starting ex3

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,6 +1,7 @@
 import math
 # import math, math.sqrt(), input(), round(), print()
 
+
 def input_function() -> tuple:
     coordinates: list = []
     while True:
@@ -8,70 +9,49 @@ def input_function() -> tuple:
             coordinates.append(float(input("= Introduce X coordinate: ")))
             print(f"X: {coordinates[0]}")
             break
-        except ValueError as error:
+        except ValueError:
             print("Invalid X coordinate. Please introduce a numerical value.")
     while True:
         try:
             coordinates.append(float(input("= Introduce Y coordinate: ")))
             print(f"Y: {coordinates[1]}")
             break
-        except ValueError as error:
+        except ValueError:
             print("Invalid Y coordinate. Please introduce a numerical value.")
     while True:
         try:
             coordinates.append(float(input("= Introduce Z coordinate: ")))
             print(f"Z: {coordinates[2]}")
             break
-        except ValueError as error:
+        except ValueError:
             print("Invalid Z coordinate. Please introduce a numerical value.")
     return tuple(coordinates)
 
 
 def coordinate_system() -> None:
-    coordinates: tuple = input_function()
+    coordinates: tuple[float, float, float] = input_function()
     label: list = ["X", "Y", "Z"]
     print(f"{coordinates}")
     i: int = 0
     for coord in coordinates:
         print(f"{label[i]}: {coord}")
         i += 1
+    print("Let's compare your coordinates with the center (0.0, 0.0, 0.0)")
+    distance_center: float
+    distance_center = math.sqrt((coordinates[0] - 0)**2
+                                + (coordinates[1] - 0)**2
+                                + (coordinates[2] - 0)**2)
+    print(f"Distante from the center is: {round(distance_center, 2)}")
+    print("Let's compare the first set of cordinates with another set")
+    new_coordinates: tuple[float, float, float] = input_function()
+    distance_coords: float
+    distance_coords = math.sqrt((new_coordinates[0] - coordinates[0])**2
+                                + (new_coordinates[1] - coordinates[1])**2
+                                + (new_coordinates[2] - coordinates[2])**2)
+    print(f"Distance between coordinates is: {round(distance_coords, 2)}")
+
 
 if __name__ == "__main__":
     print("=== Game Coordinate System ===")
     coordinate_system()
     print("End of program")
-
-
-# First, write a function get_player_pos() that:
-# • Asks the user for the new player coordinates in the format x,y,z
-# • Handles improper inputs
-# • Retries until a valid set of coordinates is provided
-# • Returns a tuple containing the player’s current 3D coordinates
-# Then your code will:
-# • Get a first set of coordinates
-# • Display the tuple then display each coordinate separately
-# • Calculate the distance to the 3D center (0, 0, 0) (see below)
-# • Get a new set of coordinates
-# • Calculate the distance between the second and the first sets of coordinates
-
-# Distance Formula: To calculate the distance between two 3D points, we use the Eu-
-# clidean distance formula
-# √
-# (x2 − x1)2 + (y2 − y1)2 + (z2 − z1)2 .
-# For points (x1, y1, z1) and (x2, y2, z2), the distance is math.sqrt((x2-x1)**2 +
-# (y2-y1)**2 + (z2-z1)**2). This is just the 3D extension of the Pythagorean theorem!
-
-# $> python3 ft_coordinate_system.py
-# === Game Coordinate System ===
-# Get a first set of coordinates
-# Enter new coordinates as floats in format 'x,y,z': hello world
-# Invalid syntax
-# Enter new coordinates as floats in format 'x,y,z': 1.0 , 2.5, 3.0
-# Got a first tuple: (1.0, 2.5, 3.0)
-# It includes: X=1.0, Y=2.5, Z=3.0
-# Distance to center: 4.0311
-# Get a second set of coordinates
-# Enter new coordinates as floats in format 'x,y,z': 4,abc,5
-# Error on parameter 'abc': could not convert string to float: 'abc'
-# Enter new coordinates as floats in format 'x,y,z': 4,5,6
-# Distance between the 2 sets of coordinates: 4.9244
